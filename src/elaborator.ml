fcfccc8c6af2c7136d125cbad3b95f9a84661935
Alexis CARRÉ <alexis.carre@ymail.com>
2023-06-13 13:34:16 +0200

    Finished implementing tactics telling whether they were successfuly applied or not

--- src/elaborator.ml
+++ src/elaborator.ml
@@ -28,9 +28,7 @@ let rec count_holes = function
 
 (* This function replace the rightmost hole by a new h_proof_term *)
 
-let rec replace_in_hpt h s r = replace_in_hpt' h s r 0
-
-and replace_in_hpt' h s r n =
+and replace_in_hpt h s r n =
   match h with
   | Hole -> (
       match r with
@@ -47,18 +45,18 @@ and replace_in_hpt' h s r n =
       | TopElim -> HUnary (s, r, Hole)
       | _ -> failwith "the rule could not replace a hole")
   | HEmpty (s', r') -> HEmpty (s', r')
-  | HUnary (s', r', h') -> HUnary (s', r', replace_in_hpt' h' s r n)
+  | HUnary (s', r', h') -> HUnary (s', r', replace_in_hpt h' s r n)
   | HBinary (s', r', h1, h2) ->
       let nb_holes = count_holes h2 in
-      if n < nb_holes then HBinary (s', r', h1, replace_in_hpt' h2 s r n)
-      else HBinary (s', r', replace_in_hpt' h1 s r (n - nb_holes), h2)
+      if n < nb_holes then HBinary (s', r', h1, replace_in_hpt h2 s r n)
+      else HBinary (s', r', replace_in_hpt h1 s r (n - nb_holes), h2)
   | HTernary (s', r', h1, h2, h3) ->
       let nb_holes = count_holes h3 in
       let nb_holes' = count_holes h2 + nb_holes in
-      if n < nb_holes then HTernary (s', r', h1, h2, replace_in_hpt' h3 s r n)
+      if n < nb_holes then HTernary (s', r', h1, h2, replace_in_hpt h3 s r n)
       else if n < nb_holes' then
-        HTernary (s', r', h1, replace_in_hpt' h2 s r (n - nb_holes), h3)
-      else HTernary (s', r', replace_in_hpt' h1 s r (n - nb_holes'), h2, h3)
+        HTernary (s', r', h1, replace_in_hpt h2 s r (n - nb_holes), h3)
+      else HTernary (s', r', replace_in_hpt h1 s r (n - nb_holes'), h2, h3)
 
 let tl = function [] -> [] | x :: xs -> xs
 
@@ -92,116 +90,139 @@ let print_error s =
   print_string "The goals are left unchanged."
 
 let apply_axiom prf_st hpt n =
-  let s, new_prf_st = pop_nth n !prf_st in
+  let s, new_prf_st = pop_nth n prf_st in
   let l, a = s in
   try
     Kernel.verif_axiom a l;
-    prf_st := insert_nth n s new_prf_st;
-    hpt := replace_in_hpt' !hpt s Axiom n
-  with _ -> print_error "the formula is not in the context"
+    let prf_st = new_prf_st and hpt = replace_in_hpt hpt s Axiom n in
+    Some (prf_st, hpt)
+  with _ ->
+    print_error "the formula is not in the context";
+    None
 
 let apply_abstraction prf_st hpt n =
-  let s, new_prf_st = pop_nth n !prf_st in
+  let s, new_prf_st = pop_nth n prf_st in
   let ctx, t = s in
   try
     let a, b = split_arrow t in
     let s' = (a :: ctx, b) in
-    prf_st := insert_nth n s' new_prf_st;
-    hpt := replace_in_hpt' !hpt s Abstraction n
-  with _ -> print_error "the formula is not an arrow"
+    let prf_st = insert_nth n s' new_prf_st
+    and hpt = replace_in_hpt hpt s Abstraction n in
+    Some (prf_st, hpt)
+  with _ ->
+    print_error "the formula is not an arrow";
+    None
 
 let apply_modus_ponens prf_st a hpt n =
-  let s, new_prf_st = pop_nth n !prf_st in
+  let s, new_prf_st = pop_nth n prf_st in
   let ctx, b = s in
-  let s1 = (ctx, Arr (a, b)) in
-  let s2 = (ctx, a) in
-  prf_st := insert_nth n s1 (insert_nth n s2 new_prf_st);
-  hpt := replace_in_hpt' !hpt s ModusPonens n
+  let s1 = (ctx, Arr (a, b)) and s2 = (ctx, a) in
+  let prf_st = insert_nth n s1 (insert_nth n s2 new_prf_st)
+  and hpt = replace_in_hpt hpt s ModusPonens n in
+  Some (prf_st, hpt)
 
 let apply_and_intro prf_st hpt n =
-  let s, new_prf_st = pop_nth n !prf_st in
+  let s, new_prf_st = pop_nth n prf_st in
   let ctx, t = s in
   try
     let a, b = split_and t in
-    let s1 = (ctx, a) in
-    let s2 = (ctx, b) in
-    prf_st := insert_nth n s1 (insert_nth n s2 new_prf_st);
-    hpt := replace_in_hpt' !hpt s AndIntro n
-  with _ -> print_error "the formula is not a conjunction"
+    let s1 = (ctx, a) and s2 = (ctx, b) in
+    let prf_st = insert_nth n s1 (insert_nth n s2 new_prf_st)
+    and hpt = replace_in_hpt hpt s AndIntro n in
+    Some (prf_st, hpt)
+  with _ ->
+    print_error "the formula is not a conjunction";
+    None
 
 let apply_and_elim prf_st f hpt n =
   match f with
   | And (a, b) ->
-      let s, new_prf_st = pop_nth n !prf_st in
+      let s, new_prf_st = pop_nth n prf_st in
       let ctx, c = s in
-      let s1 = (ctx, f) in
-      let s2 = (a :: b :: ctx, c) in
-      prf_st := insert_nth n s1 (insert_nth n s2 new_prf_st);
-      hpt := replace_in_hpt' !hpt s AndElim n
-  | _ -> print_error "the formula eliminated is not a conjunction"
+      let s1 = (ctx, f) and s2 = (a :: b :: ctx, c) in
+      let prf_st = insert_nth n s1 (insert_nth n s2 new_prf_st)
+      and hpt = replace_in_hpt hpt s AndElim n in
+      Some (prf_st, hpt)
+  | _ ->
+      print_error "the formula eliminated is not a conjunction";
+      None
 
 let apply_and_elim_left prf_st f hpt n =
-  let s = nth n !prf_st in
+  let s = nth n prf_st in
   let ctx, a = s in
-  apply_and_elim prf_st (And (f, a)) hpt n;
-  apply_axiom prf_st hpt (n + 1)
+  match apply_and_elim prf_st (And (f, a)) hpt n with
+  | None -> None
+  | Some (prf_st', hpt') -> apply_axiom prf_st' hpt' (n + 1)
 
 let apply_and_elim_right prf_st f hpt n =
-  let s = nth n !prf_st in
+  let s = nth n prf_st in
   let ctx, a = s in
-  apply_and_elim prf_st (And (a, f)) hpt n;
-  apply_axiom prf_st hpt (n + 1)
+  match apply_and_elim prf_st (And (a, f)) hpt n with
+  | None -> None
+  | Some (prf_st', hpt') -> apply_axiom prf_st' hpt' (n + 1)
 
 let apply_or_introl prf_st hpt n =
-  let s, new_prf_st = pop_nth n !prf_st in
+  let s, new_prf_st = pop_nth n prf_st in
   let ctx, t = s in
   try
     let a, b = split_or t in
     let s' = (ctx, a) in
-    prf_st := insert_nth n s' new_prf_st;
-    hpt := replace_in_hpt' !hpt s OrIntrol n
-  with _ -> print_error "the formula is not a disjunction"
+    let prf_st = insert_nth n s' new_prf_st
+    and hpt = replace_in_hpt hpt s OrIntrol n in
+    Some (prf_st, hpt)
+  with _ ->
+    print_error "the formula is not a disjunction";
+    None
 
 let apply_or_intror prf_st hpt n =
-  let s, new_prf_st = pop_nth n !prf_st in
+  let s, new_prf_st = pop_nth n prf_st in
   let ctx, t = s in
   try
     let a, b = split_or t in
     let s' = (ctx, b) in
-    prf_st := insert_nth n s' new_prf_st;
-    hpt := replace_in_hpt' !hpt s OrIntror n
-  with _ -> print_error "the formula is not a disjunction"
+    let prf_st = insert_nth n s' new_prf_st
+    and hpt = replace_in_hpt hpt s OrIntror n in
+    Some (prf_st, hpt)
+  with _ ->
+    print_error "the formula is not a disjunction";
+    None
 
 let apply_or_elim prf_st f hpt n =
   match f with
   | Or (a, b) ->
-      let s, new_prf_st = pop_nth n !prf_st in
+      let s, new_prf_st = pop_nth n prf_st in
       let ctx, c = s in
       let s1 = (ctx, f) in
       let s2 = (a :: ctx, c) in
       let s3 = (b :: ctx, c) in
-      prf_st := insert_nth n s1 (insert_nth n s2 (insert_nth n s3 new_prf_st));
-      hpt := replace_in_hpt' !hpt s OrElim n
-  | _ -> print_error "the formula eliminated is not a disjunction"
+      let prf_st =
+        insert_nth n s1 (insert_nth n s2 (insert_nth n s3 new_prf_st))
+      and hpt = replace_in_hpt hpt s OrElim n in
+      Some (prf_st, hpt)
+  | _ ->
+      print_error "the formula eliminated is not a disjunction";
+      None
 
 let apply_top_intro prf_st hpt n =
-  let s, new_prf_st = pop_nth n !prf_st in
-  prf_st := new_prf_st;
-  hpt := replace_in_hpt' !hpt s TopIntro n
+  let s, new_prf_st = pop_nth n prf_st in
+  let prf_st = new_prf_st and hpt = replace_in_hpt hpt s TopIntro n in
+  Some (prf_st, hpt)
 
 let apply_top_elim prf_st hpt n =
-  let s, new_prf_st = pop_nth n !prf_st in
+  let s, new_prf_st = pop_nth n prf_st in
   let ctx, a = s in
   let s' = (Top :: ctx, a) in
-  prf_st := insert_nth n s' new_prf_st;
-  hpt := replace_in_hpt' !hpt s TopElim n
+  let prf_st = insert_nth n s' new_prf_st
+  and hpt = replace_in_hpt hpt s TopElim n in
+  Some (prf_st, hpt)
 
 let apply_bottom_elim prf_st hpt n =
-  let s, new_prf_st = pop_nth n !prf_st in
+  let s, new_prf_st = pop_nth n prf_st in
   let ctx, a = s in
   let s' = (ctx, Bottom) in
-  prf_st := insert_nth n s' new_prf_st;
-  hpt := replace_in_hpt' !hpt s BottomElim n
+  let prf_st = insert_nth n s' new_prf_st
+  and hpt = replace_in_hpt hpt s BottomElim n in
+  Some (prf_st, hpt)
 
 (* The proof terms of the kernel are terms which do not contains hole *)
 
--- src/proplog.ml
+++ src/proplog.ml
@@ -5,11 +5,7 @@ open Parser
 open Lexing
 open Lexer
 
-let proof_state : goal ref = ref []
-let proof_tree = ref Hole
-let proof_finished = ref false
-
-let _ =
+let display_intro () =
   print_string
     "Welcome to the PropLog proof assistant. Enter the sequent that you want \
      to prove. \n\n\
@@ -17,65 +13,75 @@ let _ =
     \        A1, ..., An |- B\n\n\
     \        The Ais contain only propositional variables, parenthesis, \
      arrows, T (as top),\n\
-    \        F (as bottom), conjunction and disjunction\n";
+    \        F (as bottom), conjunction and disjunction\n"
+
+and display_help () =
+  print_string
+    "What is the next step of your proof ?\n\n\
+    \        You can :\n\n\
+    \        - Apply an axiom rule by writing 'Axiom'\n\n\
+    \        - Apply the modus ponens on a formula A by writing 'ModusPonens \
+     A'\n\n\
+    \        - Apply the abstraction rule by writing 'Abstraction'\n\n\
+    \        - Apply the and introduction rule by writing 'AndIntro'\n\n\
+    \        - Apply the and elimination rule with the two conjuncts A and B \
+     by writing 'AndElim A B'\n\n\
+    \          Or use the left or right rule by writing 'AndElimLeft A' or \
+     'AndElimRight B'\n\n\
+    \        - Apply the or introduction left rule by writing 'OrIntrol'\n\n\
+    \        - Apply the or introduction right rule by writing 'OrIntror'\n\n\
+    \        - Apply the or elimination rule with the two disjuncts A and B by \
+     writing 'OrElim A B'\n\n\
+    \        - Apply the bottom elimination rule by writing 'BottomElim'\n\n\
+    \        - Apply the top introduction rule by writing 'TopIntro'\n\n\
+    \        - Apply the top elimination rule by writing 'TopElim'\n"
+
+let get_goal () =
   let s = read_line () in
   let l = Lexing.from_string s in
-  let s = Parser.seq Lexer.token l in
-  proof_state := s :: !proof_state;
-  (* goal_to_string !proof_state *)
-  while not !proof_finished do
-    print_string
-      "What is the next step of your proof ?\n\n\
-      \        You can :\n\n\
-      \        - Apply an axiom rule by writing 'Axiom'\n\n\
-      \        - Apply the modus ponens on a formula A by writing 'ModusPonens \
-       A'\n\n\
-      \        - Apply the abstraction rule by writing 'Abstraction'\n\n\
-      \        - Apply the and introduction rule by writing 'AndIntro'\n\n\
-      \        - Apply the and elimination rule with the two conjuncts A and B \
-       by writing 'AndElim A B'\n\n\
-      \          Or use the left or right rule by writing 'AndElimLeft A' or \
-       'AndElimRight B'\n\n\
-      \        - Apply the or introduction left rule by writing 'OrIntrol'\n\n\
-      \        - Apply the or introduction right rule by writing 'OrIntror'\n\n\
-      \        - Apply the or elimination rule with the two disjuncts A and B \
-       by writing 'OrElim A B'\n\n\
-      \        - Apply the bottom elimination rule by writing 'BottomElim'\n\n\
-      \        - Apply the top introduction rule by writing 'TopIntro'\n\n\
-      \        - Apply the top elimination rule by writing 'TopElim'\n";
-    let s1 = read_line () in
-    let l1 = Lexing.from_string s1 in
-    let n, r = Parser.rule Lexer.token l1 in
-    let _ =
-      match r with
-      | None, Axiom -> Elaborator.apply_axiom proof_state proof_tree n
-      | None, Abstraction ->
-          Elaborator.apply_abstraction proof_state proof_tree n
-      | Some f, ModusPonens ->
-          Elaborator.apply_modus_ponens proof_state f proof_tree n
-      | None, AndIntro -> Elaborator.apply_and_intro proof_state proof_tree n
-      | Some f, AndElim -> Elaborator.apply_and_elim proof_state f proof_tree n
-      | Some f, AndElimLeft ->
-          Elaborator.apply_and_elim_left proof_state f proof_tree n
-      | Some f, AndElimRight ->
-          Elaborator.apply_and_elim_right proof_state f proof_tree n
-      | None, OrIntrol -> Elaborator.apply_or_introl proof_state proof_tree n
-      | None, OrIntror -> Elaborator.apply_or_intror proof_state proof_tree n
-      | Some f, OrElim -> Elaborator.apply_or_elim proof_state f proof_tree n
-      | None, TopIntro -> Elaborator.apply_top_intro proof_state proof_tree n
-      | None, TopElim -> Elaborator.apply_top_elim proof_state proof_tree n
-      | None, BottomElim ->
-          Elaborator.apply_bottom_elim proof_state proof_tree n
-      | _ -> failwith "error while applying a rule"
-    in
-    let len = List.length !proof_state in
-    Printf.printf "\nThere are %d remaining goals.\n" len;
-    match !proof_state with
-    | [] ->
-        proof_finished := true;
-        print_string "Proof finished. Call to the kernel !";
-        let ptree = hpt_to_pt !proof_tree in
-        verif_proof_term ptree;
-        print_string "\nQED.\n"
-    | x :: xs -> goal_to_string (x :: xs)
-  done
+  Parser.seq Lexer.token l
+
+and get_rule () =
+  let s = read_line () in
+  let l = Lexing.from_string s in
+  Parser.rule Lexer.token l
+
+and apply_rule state tree n r =
+  match r with
+  | None, Axiom -> Elaborator.apply_axiom state tree n
+  | None, Abstraction -> Elaborator.apply_abstraction state tree n
+  | Some f, ModusPonens -> Elaborator.apply_modus_ponens state f tree n
+  | None, AndIntro -> Elaborator.apply_and_intro state tree n
+  | Some f, AndElim -> Elaborator.apply_and_elim state f tree n
+  | Some f, AndElimLeft -> Elaborator.apply_and_elim_left state f tree n
+  | Some f, AndElimRight -> Elaborator.apply_and_elim_right state f tree n
+  | None, OrIntrol -> Elaborator.apply_or_introl state tree n
+  | None, OrIntror -> Elaborator.apply_or_intror state tree n
+  | Some f, OrElim -> Elaborator.apply_or_elim state f tree n
+  | None, TopIntro -> Elaborator.apply_top_intro state tree n
+  | None, TopElim -> Elaborator.apply_top_elim state tree n
+  | None, BottomElim -> Elaborator.apply_bottom_elim state tree n
+  | _ -> None
+
+let proof_state : goal ref = ref []
+let proof_tree = ref Hole
+let proof_finished = ref false
+let empty = function [] -> true | _ -> false
+
+let _ =
+  display_intro ();
+  proof_state := [ get_goal () ];
+  while not @@ empty !proof_state do
+    display_help ();
+    let n, r = get_rule () in
+    (match apply_rule !proof_state !proof_tree n r with
+    | None -> print_string "Invalid rule.\n"
+    | Some (state, tree) ->
+        proof_state := state;
+        proof_tree := tree);
+    Printf.printf "\nThere are %d remaining goals.\n" (List.length !proof_state);
+    goal_to_string !proof_state
+  done;
+  print_string "Proof finished. Call to the kernel !";
+  verif_proof_term @@ hpt_to_pt !proof_tree;
+  print_string "\nQED.\n"
